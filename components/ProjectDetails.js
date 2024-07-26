1609245febd8001732db6170a2a55ec5b01dfe75
rohb22 <rohbpaloma@gmail.com>
2024-07-26 11:49:03 +0800

    Implement edit project

--- components/ProjectDetails.js
+++ components/ProjectDetails.js
@@ -13,6 +13,9 @@ export default function ProjectDetails({route}) {
     const [feature, setFeature] = useState('')
     const [showEditNotes, setShowEditNotes] = useState(false)
     const [notes, setNotes] = useState("")
+    const [showEditProject, setShowEditProject] = useState(false)
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
 
 
   const saveProjects = async (projects) => {
@@ -28,8 +31,13 @@ export default function ProjectDetails({route}) {
             const savedProjects = await AsyncStorage.getItem('projects');
             if (savedProjects !== null) {
               const projectList = JSON.parse(savedProjects);
+              let selectedProject = projectList[projectIndex]
               setProjects(projectList)
-              setProject(projectList[projectIndex])
+              setProject(selectedProject)
+              if(selectedProject) {
+                setTitle(selectedProject.title)
+                setDescription(selectedProject.description || '')
+              }
             }
             } catch (error) {
             console.error('Error loading tasks:', error);
@@ -39,13 +47,6 @@ export default function ProjectDetails({route}) {
         loadProjects();
       }, []);
 
-      if (project == null) {
-        return(
-            <View style={styles.container}>
-                <ActivityIndicator style={styles.loading} size="large"/>
-            </View>
-        )
-      }
 
     const handleShowAddFeature = () => {
         setShowAddFeature(!showAddFeature)
@@ -108,12 +109,75 @@ export default function ProjectDetails({route}) {
     const handleNotesChange = (text) => {
         setNotes(text)
     }
+
+    const handleShowEditProject = () => {
+        setShowEditProject(true)
+    }
+
+    const handleEditTitleChange = (text) => {
+        setTitle(text)
+    }
     
+    const handleEditDescriptionChange = (text) => {
+        setDescription(text)
+    }
+    const handleSave = () => {
+        setShowEditProject(false)
+        Keyboard.dismiss()
+        if (title == "") {
+            setTitle(project.title)
+            return
+        }
+        const updatedProject = {...project, description: description, title: title}
+        const updatedProjects = [...projects]
+        updatedProjects[projectIndex] = updatedProject
+        setProject(updatedProject)
+        setProjects(updatedProjects)
+        saveProjects(updatedProjects)
+        
+    }
+
+    const handleCancel = () => {
+        setShowEditProject(false)
+        Keyboard.dismiss()
+        setTitle(project.title)
+        setDescription(project.description)
+    }
+
+    if (project == null) {
+        return(
+            <View style={styles.container}>
+                <ActivityIndicator style={styles.loading} size="large"/>
+            </View>
+        )
+      }
+
   return (
     <View style={styles.container}>
+        {showEditProject && (
+        <KeyboardAvoidingView style={styles.formOverlay}>
+        <View style={styles.formContainer}>
+          <View style={styles.formField}>
+            <Text style={styles.formLabel}>Project Name: </Text>
+            <TextInput style={styles.titleInput} value={title} onChangeText={handleEditTitleChange}></TextInput>
+          </View>
+          <View style={styles.formField}>
+            <Text style={styles.formLabel}>Project Description: </Text>
+            <TextInput style={styles.descriptionInput} multiline={true} placeholder='Description' value={description} onChangeText={handleEditDescriptionChange}></TextInput>
+          </View>
+          <View style={styles.formOptions}>
+            <TouchableOpacity style={styles.saveBtn} onPress={handleSave}><Text style={styles.saveTxt}>Save</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.cancelBtn} onPress={handleCancel}><Text style={styles.cancelTxt}>Cancel</Text></TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    )}
       <ScrollView showsVerticalScrollIndicator={false} keyboardShouldPersistTaps='handled'>
         <View style={styles.header}>
-            <Text style={styles.title}>{project.title}</Text>
+            <View style={styles.headerTitle}>
+                <TouchableOpacity style={styles.editNotesBtn} onPress={handleShowEditProject}><Image style={styles.deleteImg}source={require("../assets/edit.png")}/></TouchableOpacity>
+                <Text style={styles.title}>{project.title}</Text>
+            </View>
             <Text style={styles.description}>{project.description}</Text>
         </View>
         <View style={styles.label}>
@@ -140,10 +204,10 @@ export default function ProjectDetails({route}) {
                 </View>
             )}
         <View style={styles.label}>
-            <TouchableOpacity style={styles.editNotesBtn} onPress={handleEditNotes} placeholder='Make sure to click the edit icon again to save'><Image style={styles.deleteImg}source={require("../assets/edit.png")}/></TouchableOpacity>
+            <TouchableOpacity style={styles.editNotesBtn} onPress={handleEditNotes}><Image style={styles.deleteImg}source={require("../assets/edit.png")}/></TouchableOpacity>
             <Text style={styles.title}>Notes</Text>
         </View>
-        {showEditNotes ? (<TextInput style={styles.notesInput} multiline={true} value={notes} onChangeText={handleNotesChange}></TextInput>) : (<Text style={styles.notesTxt}>{project.notes}</Text>)}
+        {showEditNotes ? (<TextInput style={styles.notesInput} multiline={true} value={notes} onChangeText={handleNotesChange} placeholder='Make sure to click the edit icon again to save'></TextInput>) : (<Text style={styles.notesTxt}>{project.notes}</Text>)}
 
       </ScrollView>
     </View>
@@ -160,6 +224,10 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
     marginBottom: 10,
+  },
+  headerTitle: {
+    flexDirection: "row",
+
   },
   description: {
     marginLeft: 20,
@@ -256,5 +324,48 @@ notesTxt: {
     borderRadius: 5,
     fontSize: 15,
     padding: 10
-}
+},
+formOverlay: {
+    position: "absolute",
+    backgroundColor: "rgba(0,0,0,0.6)",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1
+  },
+    formContainer: {
+      padding: 20,
+      backgroundColor: "#fff",
+      alignItems: "left",
+      justifyContent: "space-between",
+      borderRadius: 20,
+      borderTopRightRadius: 5,
+      borderBottomLeftRadius: 5,
+      maxWidth: "85%",
+      width: "85%",
+    },
+    formField: {
+      marginBottom: 30
+    },
+    formLabel: {
+      fontSize: 20,
+      fontWeight: "bold"
+    },
+    titleInput: {
+      fontSize: 20,
+      marginLeft: 5
+    },
+    descriptionInput: {
+      padding: 5,
+      fontSize: 15,
+      marginLeft: 5,
+    },
+    formOptions: {
+      flexDirection: "row",
+      justifyContent: "space-around"
+    },
+  
 });
